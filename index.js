e2e5ffcb1cf5ced0b94939af3a4ff9c77d754407
luis henrique <luishenriquesantos122@gmail.com>
2023-11-21 22:19:05 -0300

    Deletando dados do banco e completando o CRUD

--- index.js
+++ index.js
@@ -1,42 +1,147 @@
+const { request, response } = require("express")
 const express = require("express")
 const exphbs = require("express-handlebars")
-const mysql = require("mysql")
+const mysql = require("mysql2")
 
 const app = express()
 
-// definido handlebars como template engine
-app.engine("hadlebars", exphbs.engine())
-app.set("view engine", " handlebars")
+// definindo o Handlebars como template engine
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
 
-// pasta de arquivos estaticos
+// Pasta de Arquivos estaticos como CSS, imagens
 app.use(express.static("public"))
 
-// trabalhar com dados no formato json
+// Trabalhar com dados no formato Json
 app.use(express.urlencoded({
     extended: true
 }))
+
+// CRUD => (  Create, Read, Update, Delete  )
+
 app.use(express.json())
-// rotas
-app.get('/', (requisicao, resposta) => {
-    const sql = 'SELECT * FROM books'
-    conn.query(sql, (error, data) =>{
+
+
+//Rotas
+app.post("/delete/:id", (request, response)=> {
+    const { id } = request.params
+
+    const sql =`
+    DELETE FROM books
+    WHERE id = ${id}
+    `
+    conn.query(sql, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        response.redirect("/")
+    })
+})
+
+app.post("/edit/save", (request, response) => {
+    const { id, title, pageqty } = request.body
+
+    const sql = `
+        UPDATE books
+        SET title = '${title}', pageqty = '${pageqty}'
+        WHERE id = ${id}
+    `
+
+    conn.query(sql, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        response.redirect("/")
+    })
+})
+
+app.post("/register/save", (request, response) => {
+    const { title, pageqty } = request.body
+
+    const book = 
+    {
+        title: title,
+        pageqty: pageqty
+    }
+
+    const query = `
+        INSERT INTO books (title, pageqty)
+        VALUES ('${book.title}', '${book.pageqty}')
+    `
+
+    conn.query(query, (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.redirect("/")
+    })
+})
+
+app.get("/edit/:id", (request, response) => {
+    const id = request.params.id
+
+    const sql = `
+        SELECT * FROM books
+        WHERE id = ${id}
+    `
+
+    conn.query(sql, (error, data) => {
         if (error) {
             return console.log(error)
         }
         
-        const books = data
+        const book = data[0]
 
-        console.log(books)
-
-        resposta.render("home", {books})
+        response.render('edit', { book })
     })
 })
 
-// conexão com mysql
-const conn = mysql.createConnectionConfig({
-    host:"localhost",
+app.get("/book/:id", (request, response) => {
+    const id = request.params.id
+    const sql = `
+        SELECT * FROM books 
+        WHERE id = ${id}
+    `
+
+    conn.query(sql, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+       const book = data[0]
+
+       response.render("book", { book })
+    })
+})
+
+app.get("/register", (request, response) => {
+    response.render("register")
+})
+
+app.get("/", (resquest, response) => {
+    const sql = 'SELECT * FROM books'
+
+    conn.query(sql, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        const books = data
+
+        response.render("home", { books })
+    })
+})
+
+
+//Conexão com MySQL
+const conn = mysql.createConnection({
+    host: "localhost",
     user: "root",
-    passaword: "root",
+    password: "root",
     database: "nodemysql",
     port: 3307
 })
@@ -46,8 +151,10 @@ conn.connect((error) => {
         console.log(error)
         return
     }
-    console.log("Conectado ao MySQL")
+
+    console.log("Conectado ao MySQL!")
+
     app.listen(3000, () => {
-        console.log(" Servidor rodando na porta 3000!")
+        console.log("Servidor rodando na Porta 3000!")
     })
 })
